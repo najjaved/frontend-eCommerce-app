60610ec163a2ab64243cfd4ae9956f067edd6d5a
maria magdalena <neumariamagdalena@gmail.com>
2024-07-06 21:35:41 +0200

    make card on homepage make sense, populate with info & placeholder, make button transform into <a> tag and guide to listpage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,32 +1,37 @@
 import { Link } from "react-router-dom";
+
 import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
 import placeholder from "../assets/placeholderList.jpeg";
 function HomePage() {
   return (
     <>
       <h1> Welcome</h1>
-      <Link to="/ListPage">
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <Card.Section>
-            <Image src={placeholder} height={160} alt="Norway" />
-          </Card.Section>
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Card.Section>
+          <Image src={placeholder} height={160} alt="Norway" />
+        </Card.Section>
 
-          <Group justify="space-between" mt="md" mb="xs">
-            <Text fw={500}>Norway Fjord Adventures</Text>
-            <Badge color="pink">On Sale</Badge>
-          </Group>
+        <Group justify="space-between" mt="md" mb="xs">
+          <Text fw={500}>Herbal Product List</Text>
+          <Badge color="pink">On Sale</Badge>
+        </Group>
 
-          <Text size="sm" c="dimmed">
-            With Fjord Tours you can explore more of the magical fjord
-            landscapes with tours and activities on and around the fjords of
-            Norway
-          </Text>
+        <Text size="sm" c="dimmed">
+          Nature has everything it needs to heal you and prevent you from
+          deteriorating :D BLABLABLA
+        </Text>
 
-          <Button color="blue" fullWidth mt="md" radius="md">
-            Book classic tour now
-          </Button>
-        </Card>
-      </Link>
+        <Button
+          component="a"
+          color="blue"
+          href="/ListPage"
+          fullWidth
+          mt="md"
+          radius="md"
+        >
+          Take a look at our products
+        </Button>
+      </Card>
     </>
   );
 }
