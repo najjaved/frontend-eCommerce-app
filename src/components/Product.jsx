6d27dd60c78efbba475700c2a6e4f5881372f53c
maria magdalena <neumariamagdalena@gmail.com>
2024-07-08 10:09:56 +0200

    fix typo n delet

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,41 +1,64 @@
-const Product = ({product, deletProduct, editProduct}) => {
+const Product = ({ product, deleteProduct, editProduct }) => {
+  const handleDeleteClick = (event) => {
+    event.preventDefault();
+    deleteProduct(product.product_id);
+  };
 
-    const handleDeleteClick = (event) => {
+  const handleEditClick = (event) => {
+    event.preventDefault();
+    editProduct(product.product_id);
+  };
 
-      event.preventDefault();
-      deletProduct(product.product_id);
-    };
-
-    const handleEditClick = (event) => {
-        event.preventDefault();
-        editProduct(product.product_id);
-      };
-
-
-    return ( 
-        <div className = "product-div">
-          <div className="images-div">
-            <img src= {product.images[0]} alt = 'image to be added' /> 
-          </div>
-          <div className='details-div'> 
-            <p> <b>Product Category:</b> {product.category} </p>
-            <p> <b>Title:</b> {product.product_name} </p>
-            <p> <b>Price:</b> {product.price} </p>
-            <p> <b>Discount:</b> {product.discount}</p>
-            {product.price <30 && <span style={{color: '#3B82F6', fontWeight:'bold'}}> Great Deal </span>}
-            <div className="buttons-div">
-              <button className= 'btn-primary' type = "button" onClick={handleDeleteClick} > Delete </button> {/* replace with mantine buttons  */}
-              <button className= 'btn-secondary' type = "button" onClick={handleEditClick}> Edit </button>
-            </div>
-          </div>
+  return (
+    <div className="product-div">
+      <div className="images-div">
+        <img src={product.images[0]} alt="image to be added" />
+      </div>
+      <div className="details-div">
+        <p>
+          {" "}
+          <b>Product Category:</b> {product.category}{" "}
+        </p>
+        <p>
+          {" "}
+          <b>Title:</b> {product.product_name}{" "}
+        </p>
+        <p>
+          {" "}
+          <b>Price:</b> {product.price}{" "}
+        </p>
+        <p>
+          {" "}
+          <b>Discount:</b> {product.discount}
+        </p>
+        {product.price < 30 && (
+          <span style={{ color: "#3B82F6", fontWeight: "bold" }}>
+            {" "}
+            Great Deal{" "}
+          </span>
+        )}
+        <div className="buttons-div">
+          <button
+            className="btn-primary"
+            type="button"
+            onClick={handleDeleteClick}
+          >
+            {" "}
+            Delete{" "}
+          </button>{" "}
+          {/* replace with mantine buttons  */}
+          <button
+            className="btn-secondary"
+            type="button"
+            onClick={handleEditClick}
+          >
+            {" "}
+            Edit{" "}
+          </button>
         </div>
-       );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Product;
-
-
-
-
-
-
--- src/pages/ProductsListPage.jsx
+++ src/pages/ProductsListPage.jsx
@@ -1,38 +1,43 @@
-import { Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import Product from "../components/Product";
 
-const ProductsListPage = ({productsList, setProductsList}) =>{
-
+const ProductsListPage = ({ productsList, setProductsList }) => {
   const navigate = useNavigate();
 
   //deletProduct(product.id) called from Product.jsx
   const handleDelete = (id) => {
-    setProductsList(productsList.filter(currentProduct => (currentProduct.product_id !== id)));
-  }
+    setProductsList(
+      productsList.filter((currentProduct) => currentProduct.product_id !== id)
+    );
+  };
 
   const handleEdit = (id) => {
     navigate(`/products/edit/${id}`); // Navigate to edit page
-  
-  }
+  };
 
   return (
     <div className="ProductListPage">
       <h1>Listpage to be styled</h1>
-      <Link to='/products/newProduct'> 
-            <h2>Add Product</h2>
+      <Link to="/products/newProduct">
+        <h2>Add Product</h2>
       </Link>
-      {productsList && productsList.map((currentProduct) => {
-        return (
-          <div className="products-grid" key={currentProduct.product_id}>
-            <Link to={`/products/${currentProduct.product_id}`}>
-            <Product product = {currentProduct} deletProduct ={handleDelete} editProduct = {handleEdit} />
-            </Link>
-          </div>
-        );
-      })}
+      {productsList &&
+        productsList.map((currentProduct) => {
+          return (
+            <div className="products-grid" key={currentProduct.product_id}>
+              <Link to={`/products/${currentProduct.product_id}`}>
+                <Product
+                  product={currentProduct}
+                  deleteProduct={handleDelete}
+                  editProduct={handleEdit}
+                />
+              </Link>
+            </div>
+          );
+        })}
     </div>
   );
-}
+};
 
 export default ProductsListPage;
