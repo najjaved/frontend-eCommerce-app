115b567d05ae90ad581963d89fcbcbb29d0ad9a9
maria magdalena <neumariamagdalena@gmail.com>
2024-07-11 10:49:39 +0200

    add info about maria on about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,46 +1,70 @@
-import profileIcon from "../assets/images/profile-icon.png"
+import profileIcon from "../assets/images/profile-icon.png";
 import { Link } from "react-router-dom";
-import '../styles/AboutPage.css';
+import "../styles/AboutPage.css";
 
 const AboutPage = () => {
-    return ( 
+  return (
     <div className="about-page">
+      <h3>About Page</h3>
+      <p>
+        {" "}
+        This application is designed to help eCommerce admins to manage products
+        and inventory in their stores, with a dashboard displaying list of
+        products and options to add, edit or delete product(s).
+      </p>
+      <div>
+        <h5>The team</h5>
+        <p>
+          <a
+            href="https://github.com/mariamagneu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <strong>Maria Magdalena</strong>
+          </a>
+          : MA in Political Museums, took a turn into Web3 in 2021, junior
+          webDev. Maria's strenghts lie in communication & problem solving
+        </p>
+        <p>
+          <a
+            href="https://github.com/najjaved"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <strong>Najma Javed</strong>
+          </a>
+          : Electrical Engineering, Masters, HiL Test Engineer, a travel
+          enthusiast and a nature lover
+        </p>
+      </div>
 
-        <h3>About Page</h3>
-        <p> This application is designed to help eCommerce admins to manage products and inventory in their stores, 
-          with a dashboard displaying list of products and options to add, edit or delete product(s).</p>
-        <div>
-            <h5>The team</h5>
-            <p><a href="https://github.com/mariamagneu" target="_blank" rel="noopener noreferrer"><strong>Maria Magdalen</strong></a>: Experienced V...... Skilled in:
-              - xx
-              - xx
-              - xxx </p>
-            <p><a href="https://github.com/najjaved" target="_blank" rel="noopener noreferrer"><strong>Najma Javed</strong></a>: Electrical Engineering, Masters, HiL Test Engineer, a travel enthusiast and a nature lover</p>
-          
-        </div>
+      <div className="Lkdn-links">
+        <p className="xx">
+          <strong>LinkedIn:</strong>
+          <a
+            href="https://www.linkedin.com/in/maria-magdalena-neu-85845b141/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Maria,{" "}
+          </a>
+          <a
+            href="https://de.linkedin.com/in/najaved"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Najma,{" "}
+          </a>
+        </p>
+      </div>
 
-        <div className="Lkdn-links">
-              <p className="xx">
-                <strong>LinkedIn:</strong>
-                <a href='' target="_blank" rel="noopener noreferrer">Maria, </a>
-                <a href='https://de.linkedin.com/in/najaved' target="_blank" rel="noopener noreferrer"> Najma, </a>
-              </p>
-
-        </div>
-
-        {/* Back button */}
-        <Link to = "/">
-            <button type = "button"> Home </button>
-        </Link>
-
+      {/* Back button */}
+      <Link to="/">
+        <button type="button"> Home </button>
+      </Link>
     </div>
+  );
+};
 
-
-     );
-}
- 
 export default AboutPage;
-
-
-    
-      
