bb2021959579c2c38c7f1d52e42f74d8d6b8e7c5
maria magdalena <neumariamagdalena@gmail.com>
2024-07-11 12:10:40 +0200

    modal add in cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ShopContext } from "../helpers/context/shop-context";
 import CartItem from "../components/CartItem";
-import { Button } from "@mantine/core";
+import { Modal, Button } from "@mantine/core";
 import image from "../assets/images/placeholderList.jpeg";
+import naturefrau from "../assets/images/naturfrau.jpg";
+
+import { useDisclosure } from "@mantine/hooks";
 
 const Cart = () => {
   const { getTotalCartAmount, products, cartItems, fakePayReset } =
     useContext(ShopContext);
+
   const totalAmount = getTotalCartAmount();
+
+  const [modalOpened, { open, close }] = useDisclosure(false);
+  const [showEmptyCartMessage, setShowEmptyCartMessage] = useState(false);
+
+  const handleFakePayReset = () => {
+    fakePayReset();
+    open();
+  };
+
+  const handleModalClose = () => {
+    close();
+    // Set showEmptyCartMessage to true only if the cart is empty
+    setShowEmptyCartMessage(totalAmount <= 0);
+  };
 
   return (
     <div className="cart">
@@ -25,32 +43,58 @@ const Cart = () => {
       <div className="cart-checkout">
         {totalAmount > 0 ? (
           <p className="subtotal">Subtotal: ${totalAmount.toFixed(2)}</p>
-        ) : (
-          <div className="emptycart">
-            <h1 className="carth1">Your Shopping Cart is empty</h1>
-            <img className="emptyimage" src={image} alt="Placeholder" />
-          </div>
-        )}
+        ) : null}
+
         <div className="cart-buttons">
-          <Button
-            variant="filled"
-            color="indigo"
-            size="lg"
-            radius="md"
-            component="a"
-            href="/products"
+          {totalAmount <= 0 && showEmptyCartMessage && (
+            <div className="emptycart">
+              <h1 className="carth1">Your Shopping Cart is empty</h1>
+              <img className="emptyimage" src={image} alt="Placeholder" />
+            </div>
+          )}
+
+          {totalAmount > 0 && (
+            <>
+              <Button
+                variant="filled"
+                color="indigo"
+                size="lg"
+                radius="md"
+                component="a"
+                href="/products"
+              >
+                Continue Shopping
+              </Button>
+
+              <Button
+                variant="filled"
+                color="indigo"
+                size="lg"
+                radius="md"
+                onClick={handleFakePayReset}
+              >
+                Fake Pay & Reset
+              </Button>
+            </>
+          )}
+
+          <Modal
+            opened={modalOpened}
+            onClose={handleModalClose}
+            title="Payment Successful!"
+            centered
           >
-            Continue Shopping
-          </Button>
-          <Button
-            variant="filled"
-            color="indigo"
-            size="lg"
-            radius="md"
-            onClick={fakePayReset}
-          >
-            Fake Pay & Reset
-          </Button>
+            <>
+              <h1>
+                Thank you for paying & supporting this women-owned business
+              </h1>
+              <img
+                className="emptyimage"
+                src={naturefrau}
+                alt="Picture with woman in nature"
+              />
+            </>
+          </Modal>
         </div>
       </div>
     </div>
